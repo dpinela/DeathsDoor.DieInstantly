0f279350613d92243b51b638758f2e865e5d5044
Diogo Pinela <diogoid7400@gmail.com>
2024-05-26 21:54:51 +0100

    Add some explanation for the NRE's that turn up in logs from this mod

--- DieInstantly/DelegateButton.cs
+++ DieInstantly/DelegateButton.cs
@@ -4,6 +4,11 @@ internal class DelegateButton : UIButton
 {
     private System.Action action = () => {};
 
+    // Adding this component to a button causes a NullReferenceException
+    // to be thrown because Init has not been called yet to fill in its
+    // fields. This is not an issue in practice, but is worth noting for
+    // anyone who notices it in the logs.
+
     public void Init(UIButton btn, System.Action action)
     {
         highlighter = btn.highlighter;
